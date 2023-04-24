99178e17ab0c2b3a9056708bd70b6292bcd34449
Felix von Perger <22255236@student.uwa.edu.au>
2023-04-24 12:15:28 +0800

    Fix deploy script

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ DEST_DIR=dist
 
 echo "Generating output in $(pwd)/$DEST_DIR/"
 rm -rf $DEST_DIR
-mkdir -p $DEST_DIR
+mkdir -p $DEST_DIR/src
 
 ./manage.py collectstatic --noinput
 
 # Copy files to dist dir
 rsync -ru --del --exclude '__pycache__/' --exclude '*.pyc' --exclude "app/settings*.py" --exclude '*.sqlite3' \
- app database LICENSE README.md requirements.txt "$DEST_DIR"
+ app database static manage.py LICENSE README.md requirements.txt "$DEST_DIR/src"
 
+rsync -ru --del --exclude '__pycache__/' --exclude '*.pyc' --exclude "app/settings*.py" --exclude '*.sqlite3' \
+ staticfiles LICENSE README.md requirements.txt "$DEST_DIR"
